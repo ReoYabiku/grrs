62c2d375000be0c8a0eb337cfb215842fc4090ec
ReoYabiku <alcremikyu.z@gmail.com>
2023-07-28 02:50:49 +0900

    accosiated function

--- src/main.rs
+++ src/main.rs
@@ -4,25 +4,20 @@ struct Rectangle {
 }
 
 impl Rectangle {
-    fn can_hold(&self, other: &Rectangle) -> bool {
-        self.width > other.width && self.height > other.height
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+    
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size
+        }
     }
 }
 
 fn main() {
-    let rect1 = Rectangle {
-        width: 30,
-        height: 50
-    };
-    let rect2 = Rectangle {
-        width: 20,
-        height: 60
-    };
-    let rect3 = Rectangle {
-        width: 10,
-        height: 10
-    };
+    let sq = Rectangle::square(10);
 
-    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
-    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+    println!("the area is {}", sq.area());
 }
