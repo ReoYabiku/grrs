4df48140fc809f00d88dc663eba138c0e701d4a7
ReoYabiku <alcremikyu.z@gmail.com>
2023-08-01 13:54:45 +0900

    mutable vector

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
 fn main() {
-    let v = vec![1, 2, 3, 4, 5];
-    let third: i32 = v[2];
-    println!("The third element is {third}.");
+    let mut v = vec![100, 32, 63];
+    for i in &mut v {
+        *i += 50;
+    }
 
-    let third = v.get(2);
-    match third {
-        Some(third) => println!("The third element is {third}."),
-        None => println!("There is no third element."),
+    for i in &v {
+        println!("{i}");
     }
 }
