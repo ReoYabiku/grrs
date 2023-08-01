98f405841e05640a4cc87fae5e26a7d9f42e05b5
ReoYabiku <alcremikyu.z@gmail.com>
2023-08-01 13:45:33 +0900

    imutable iteration for vactor

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,11 @@
-mod back_of_house {
-    pub struct Breakfast {
-        pub toast: String,
-        seasonal_fuit: String,
-    }
-
-    impl Breakfast {
-        pub fn summer(toast: &str) -> Breakfast {
-            Breakfast {
-                toast: String::from(toast),
-                seasonal_fuit: String::from("peaches"),
-            }
-        }
-    }
-}
-
-pub fn eat_at_restaurant() {
-    let mut meal = back_of_house::Breakfast::summer("Rye");
-    meal.toast = String::from("Wheet");
-    println!("I'd like {} toast please!", meal.toast);
-}
-
 fn main() {
-    eat_at_restaurant();
+    let v = vec![1, 2, 3, 4, 5];
+    let third: i32 = v[2];
+    println!("The third element is {third}.");
+
+    let third = v.get(2);
+    match third {
+        Some(third) => println!("The third element is {third}."),
+        None => println!("There is no third element."),
+    }
 }
